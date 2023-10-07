647e6454a7cca06356be2cf8b7b196033d537c4e
Gabriel Raposo <gabriel.robertor@gmail.com>
2023-10-07 15:59:31 -0700

    feat(projects): adds wip page for project

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -2,8 +2,13 @@ import { Navigation } from '@/components';
 
 export default function Projects() {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0'>
       <Navigation />
+      <div className='container flex items-center justify-center min-h-screen h-full px-4 mx-auto'>
+        <h1 className='z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-display sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text'>
+          Coming Soon
+        </h1>
+      </div>
     </main>
   );
 }
